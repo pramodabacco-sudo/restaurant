6cc8d358aeb52a76b2241c394dbbc3f73bc9dd08
pramod <pramod.abacco@gmail.com>
2026-07-06 11:12:31 +0530

    update

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.send("Server is live 🚀 ");
+})
+
 // Routes
 // app.use("/api/auth", authRoutes);
 // app.use("/api/payments", paymentRoutes);
